d61c0c5a20fd713ca9c346d80f21f95cfabad1d5
stevenbertolucci <steven.bertolucci@yahoo.com>
2024-08-25 13:26:10 -0700

    Added a menu option

--- Auto-update.sh
+++ Auto-update.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
 
-: '
-Copyright 2024 Steven Bertolucci
+# Replace the name of the 'your_path_to_the_password_file_goes_here' text below with the name of the file that contains your password
+PASSWORD_FILE="your_path_to_the_password_file_goes_here.txt"
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-'
-
-# Echo message
-echo "This script will update and upgrade your system. You will be prompted for your sudo password."
+# Read the password from the file
+PASSWORD=$(cat "PASSWORD_FILE")
 
 # Update and upgrade the system
-gnome-terminal -- bash -c "sudo apt update && sudo apt upgrade -y; exec bash"
+gnome-terminal -- bash -c "echo $PASSWORD | sudo -S apt update && sudo apt upgrade -y; exec bash"
+
+# Display menu
+menu() {
+    echo "What do you want to do, Bud? (Pick an option below)"
+    echo "1) Check disk usage"
+    echo "2) List all files in the current directory"
+    echo "3) Display system information"
+    echo "4) Play a YouTube video"
+    echo "5) Exit"
+}
+
+# Function to handle user input
+handle_input() {
+    case $1 in
+        1)
+            echo "Checking disk usage..."
+            df -h
+            ;;
+        2)
+            echo "Listing all files in the current directory..."
+            ls -al
+            ;;
+        3)
+            echo "Displaying system information..."
+            uname -a
+            ;;
+        4)
+            echo "Opening browser to play YouTube video..."
+            YOUTUBE_URL="https://www.youtube.com/watch?v=nnkyInAj6Z8"
+            xdg-open "$YOUTUBE_URL"
+            ;;
+        5)
+            echo "Exiting. Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please try again."
+            ;;
+    esac
+}
+
+# Menu loop
+while true; do
+    display_menu
+    read -p "Enter your choice [1-5]: " choice
+    handle_input $choice
+    echo
+done
